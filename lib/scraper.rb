3666e22bdf0c411f62c9a7180b9343ba850cfdf1
Dan Foley <dan@dan-foley.com>
2020-02-13 21:58:35 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,46 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
+
+
 
 class Scraper
 
+
+# name:         doc.css(".student-card")[0].css("h4").text
+# location:     doc.css(".student-card")[0].css(".student-location").text
+
+# profile_url:  doc.css(".student-card")[0].css('a')[0].values.join
+#        -or-   doc.css(".student-card")[0].css('a').first.attributes['href'].value
+#               doc.css(".student-card")[0].css('a')[0].attributes['href'].value
+
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    student_arr = []
+    doc.css('.student-card').each do |student|
+      student_hash = {
+        :name => student.css('h4').text,
+        :location => student.css(".student-location").text,
+        :profile_url => student.css('a').first.attributes['href'].value
+      }
+      student_arr << student_hash
+    end
+    student_arr
   end
+  
+  # url:          doc.search('.social-icon-container')[0].children.css("a")[0].attributes['href'].value
+  # social-asset: doc.search('.social-icon-container')[0].children.css(".social-icon")[0].attributes["src"].value
+  # "../assets/img/twitter-icon.png".split(Regexp.union("../assets/img/","-icon.png")).reject(&:empty?).join
+  
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(profile_url))
+    # binding.pry
+    # doc.search
+    doc.search('.social-icon-container') do |social|
+      binding.pry
+    end
   end
 
 end
