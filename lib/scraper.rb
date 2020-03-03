8794fe460fe2bbb9442f20e4936b8f349fb086dd
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-03-03 05:14:01 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,12 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
+  def self.scrape_index_page(index_url) #scraping index page that lists all students
+
   end
 
-  def self.scrape_profile_page(profile_url)
-    
+  def self.scrape_profile_page(profile_url) #scrapes individual student profiles
+
   end
 
 end
-
